64ebe65a8c9e4f959e62165279fe82711233e49f
rafaelmonroy <rafaelmonroy1990@gmail.com>
2019-11-27 19:36:25 -0800

    fetching data from server

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
-function Home() {
+import React from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  // async function getProducts() {
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // }
+
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  //fetch data on server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return { products: response.data };
+  //return data as an object
+  //note: this object will be merged with existing props
+};
 
 export default Home;
